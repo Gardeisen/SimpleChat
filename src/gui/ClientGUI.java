91ce45cce45c203965ae69f8585a2f25ad4b1692
Johan Brunet <brunet.johan.23@gmail.com>
2017-12-15 16:42:04 +0100

    GUI working
    Layout to make prettier

--- src/gui/ClientGUI.java
+++ src/gui/ClientGUI.java
@@ -2,51 +2,82 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import client.*;
 import common.*;
 
-public class ClientGUI extends JFrame implements ChatIF, ActionListener{
+public class ClientGUI extends JFrame implements ChatIF, ActionListener {
 
 	final public static int DEFAULT_PORT = 5555;
-
+	private static int id;
+	
 	ChatClient client;
 
 	private JButton[] boutons = new JButton[10];
+	JTextArea chat;
 
 
 	public ClientGUI(int id, String host, int port) {
-		// Connection
-		try {
-			client = new ChatClient(id, host, port, this);
-		} 
-		catch(IOException exception) {
-			System.out.println("Error: Can't setup connection!"
-					+ " Terminating client.");
-			System.exit(1);
-		}
-
+		
+		setTitle("ClientConsole");
+		setSize(500, 600);
+		int windowWidth = (int) this.getSize().getWidth();
+		int windowHeight = (int) this.getSize().getHeight();
+		
 		// SetUp
 		Container contentPane = getContentPane();
+			
+		// Display messages
+		chat = new JTextArea();
+		JScrollPane scroll = new JScrollPane(chat, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		//scroll.setMinimumSize(new Dimension(windowWidth, windowHeight/2));
+		chat.setSize(new Dimension(windowWidth, windowHeight/2));
+		contentPane.add(scroll,BorderLayout.NORTH);
+		
+		//message
+		JPanel sendMsge = new JPanel();
+		sendMsge.setLayout(new GridLayout(1,2));
+		sendMsge.setMaximumSize(new Dimension(windowWidth, windowHeight/8));
+
+		JTextField message = new JTextField();
+		int size = sendMsge.getWidth();
+		System.out.println("Size : " + 3*size/4);
+		message.setMaximumSize(new Dimension((int) 3*sendMsge.getWidth()/4, sendMsge.getHeight()));
+		sendMsge.add(message, BorderLayout.WEST);
+		JButton buttonSend = new JButton("Send");
+		buttonSend.setActionCommand("send");
+
+		buttonSend.addActionListener( new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				chat.append("> "+message.getText()+"\n");
+				Object source = e.getSource();
+				client.handleMessageFromClientUI(message.getText());
+			}
+		});
+		buttonSend.setMaximumSize(new Dimension(sendMsge.getWidth()/4, sendMsge.getHeight()));
+		sendMsge.add(buttonSend, BorderLayout.EAST);
+		contentPane.add(sendMsge,BorderLayout.CENTER);
+		//TODO effacer le jtextfield
+		//TODO appened
+		
+		
 		JPanel panelButtons = new JPanel();
 		int n = 5;
 		panelButtons.setLayout(new GridLayout(0,n));
+		panelButtons.setMaximumSize(new Dimension(windowWidth, windowHeight/8));
 
 		//Settings
 		JButton buttonSettings = new JButton("SETTINGS");
@@ -82,44 +113,30 @@ public class ClientGUI extends JFrame implements ChatIF, ActionListener{
 		buttonQuit.setActionCommand("#quit");
 		buttonQuit.addActionListener(this);
 		panelButtons.add(buttonQuit);
-		
-		//message
-		JPanel sendMsge = new JPanel();
-		JTextField message = new JTextField();
-		sendMsge.setLayout(new GridLayout(0,2));
-		sendMsge.add(message);
-		JButton buttonSend = new JButton("SEND");
-		buttonSend.setActionCommand("send");
-
-		JTextArea chat = new JTextArea();
-		contentPane.add(chat,BorderLayout.NORTH);
-		buttonSend.addActionListener( new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				chat.setText("> "+message.getText()+"\n");
-				Object source = e.getSource();
-				client.handleMessageFromClientUI(((JButton)source).getActionCommand());
-			}
-		}
-		);
-		sendMsge.add(buttonSend);
-		//TODO effacer le jtextfield
-		//TODO appened
-		
 		contentPane.add(panelButtons,BorderLayout.SOUTH);
-		contentPane.add(sendMsge,BorderLayout.CENTER);
+		
+		// Connection
+		try {
+			client = new ChatClient(id, host, port, this);
+		} 
+		catch(IOException exception) {
+			System.out.println("Error: Can't setup connection!"
+					+ " Terminating client.");
+			System.exit(1);
+		}
 
-		setTitle("ClientConsole");
-		setSize(400, 400);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
+	
+	@Override
 	public void display(String message) {
-		System.out.println("> " + message);
-	}
+		if (!message.contains("Client " + id)) {
+			chat.append(message + "\n");
+		}
+	}	
 
 	public static void main(String[] args) {
-		int id = 0;
 		String host = "";
 		int port =0;  //The port number
 
@@ -143,8 +160,8 @@ public class ClientGUI extends JFrame implements ChatIF, ActionListener{
 			}
 			else{
 				client.handleMessageFromClientUI(((JButton)source).getActionCommand());
+				//chat.append();
 			}
 		}
 	}
-
 }
